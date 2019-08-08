49d8aa6ec08d4e81fa7724a275d0431ceb8786fc
morihiro1978 <morihiro1978@gmail.com>
2019-08-09 02:23:47 +0900

    Tokenizer 構造体を作成した。Tokenizer::next() でトークンごとに切り分けた文字を返す。トークン種別は未対応。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,135 @@
+#[derive(Debug)]
+struct Tokenizer<'a> {
+    code: &'a str,
+    chars: std::iter::Peekable<std::str::Chars<'a>>,
+}
+
+impl<'a> Tokenizer<'a> {
+    pub fn new(code: &str) -> Tokenizer {
+        Tokenizer {
+            code: &code,
+            chars: code.chars().peekable(),
+        }
+    }
+
+    pub fn next(&mut self) -> Option<String> {
+        let mut tok = String::new();
+
+        loop {
+            if let Some(c) = self.chars.next() {
+                match c {
+                    // >, >=, <, <=, =, ==, !, !=
+                    c @ '>' | c @ '<' | c @ '=' | c @ '!' => {
+                        tok.push(c);
+                        if self.chars.peek() == Some(&'=') {
+                            tok.push(self.chars.next().unwrap());
+                        }
+                        break;
+                    }
+                    // +, ++
+                    c @ '+' => {
+                        tok.push(c);
+                        if self.chars.peek() == Some(&'+') {
+                            tok.push(self.chars.next().unwrap());
+                        }
+                        break;
+                    }
+                    // -, --
+                    c @ '-' => {
+                        tok.push(c);
+                        if self.chars.peek() == Some(&'-') {
+                            tok.push(self.chars.next().unwrap());
+                        }
+                        break;
+                    }
+                    // 1 char sign
+                    c @ '*'
+                    | c @ '/'
+                    | c @ '('
+                    | c @ ')'
+                    | c @ ';'
+                    | c @ '{'
+                    | c @ '}'
+                    | c @ ',' => {
+                        tok.push(c);
+                        break;
+                    }
+                    // char
+                    '\'' => {
+                        tok.push('\'');
+                        if self.chars.peek() != Some(&'\'') {
+                            tok.push(self.chars.next().unwrap());
+                            if self.chars.peek() != Some(&'\'') {
+                                panic!("After ' is wrong.");
+                            }
+                        }
+                        tok.push(self.chars.next().unwrap());
+                        break;
+                    }
+                    // String
+                    '"' => {
+                        tok.push('"');
+                        loop {
+                            let c = self.chars.next().unwrap();
+                            tok.push(c);
+                            if c == '"' {
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                    // num
+                    c @ '0'...'9' => {
+                        tok.push(c);
+                        loop {
+                            match self.chars.peek() {
+                                Some(_c @ '0'...'9') => tok.push(self.chars.next().unwrap()),
+                                _ => break,
+                            }
+                        }
+                        break;
+                    }
+                    // ident
+                    c @ 'a'...'z' | c @ 'A'...'Z' => {
+                        tok.push(c);
+                        loop {
+                            match self.chars.peek() {
+                                Some(_c @ '0'...'9') | Some(_c @ 'a'...'z')
+                                | Some(_c @ 'A'...'Z') => tok.push(self.chars.next().unwrap()),
+                                _ => break,
+                            }
+                        }
+                        break;
+                    }
+                    c if c.is_whitespace() => {
+                        if !tok.is_empty() {
+                            break;
+                        }
+                    }
+                    _ => {
+                        break;
+                    }
+                };
+            } else {
+                break;
+            }
+        }
+
+        if tok.is_empty() {
+            None
+        } else {
+            Some(tok)
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let code: String = std::env::args().nth(1).unwrap();
+    let mut tok = Tokenizer::new(&code);
+
+    println!("code: {}", code);
+    println!("tok: {:?}", tok);
+    while let Some(c) = tok.next() {
+        println!("tok.next: {}", c);
+    }
 }
